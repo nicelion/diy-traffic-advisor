7b37431027ebd94aa3491b2729b6b69133f41e7f
Ian Thompson <ianthompson@nicelion.com>
2017-07-19 01:25:55 -0400

    Add files via upload

--- traffic-advisor.ino
+++ traffic-advisor.ino
@@ -1,10 +1,12 @@
-// lights.ino
+// traffic-advisor.ino
 // Part of the diy-traffic-advisor project
 //
 // By Ian Thompson
 // Copyright (c) 2017.
 //
-#include <MultiShiftRegister.h>
+
+#include "MultiShiftRegister.h"
+#include "Timer.h"
 
 #define ACTIVATED LOW
 
@@ -16,6 +18,8 @@ byte data = 0b00000000;
 
 MultiShiftRegister msr (2, latchPin, clockPin, dataPin);
 
+Timer timer;
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -23,25 +27,55 @@ void setup() {
   pinMode(latchPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
   pinMode(dataPin, OUTPUT);
-  
-  
+  pinMode(2, INPUT);
+
+
   msr.shift();
 
   Serial.begin(9600);
 
+  timer.every(10, check_btn_state);
 
-  
 
 }
 
 void loop() {
+
+//  if (check_btn_state(1)) {
+//    flash_all(100);
+//  }
+
+   timer.update();
   
-
-left_half(140);
-
 
 }
 
+
+
+void check_btn_state() {
+
+
+
+  /*
+  
+  This function will check every button state, and return a boolean
+
+  */
+
+  int buttonState = digitalRead(2);
+
+
+
+  if (buttonState == 1){    
+
+    l_l_r_r_lrlr(60, 2);
+
+  } else {
+
+  }
+
+  
+}
 
 /*
 
@@ -50,32 +84,39 @@ left_half(140);
     l = Left
     r = Right
     f = Full
-    etc. 
+    etc.
 
 */
 
-void l_l_r_r_f_f(int speed){
+void l_l_r_r_f_f(int speed, int repeat){
 
   // Left Left Right Right Full Full
 
-  left_half(speed);
-  delay(speed);
-  left_half(speed);
-  delay(speed);
-
-  right_half(speed);
-  delay(speed);
-  right_half(speed);
-  delay(speed);
-
-  flash_all(speed);
-  delay(speed);
-  flash_all(speed);
-  delay(speed * 1.5);
-
+  for (int i = repeat; i > 0; i--){
 
   
-  
+
+      left_half(speed);
+      delay(speed);
+      left_half(speed);
+      delay(speed);
+    
+      right_half(speed, 1);
+      delay(speed);
+      right_half(speed, 1);
+      delay(speed);
+    
+      flash_all(speed);
+      delay(speed);
+      flash_all(speed);
+      delay(speed * 1.5);
+
+
+  }
+
+
+
+
 }
 
 
@@ -86,34 +127,39 @@ void l_r_l_r(int speed){
   left_half(speed);
   delay(speed);
 
-  right_half(speed);
+  right_half(speed, 1);
   delay(speed);
-  right_half(speed);
+  right_half(speed, 1);
   delay(speed);
 }
 
-void l_l_r_r_lrlr(int speed){
-
-  left_half(speed);
-  delay(speed);
-  left_half(speed);
-  delay(speed);
-
-  right_half(speed);
-  delay(speed);
-  right_half(speed);
-  delay(speed);
+void l_l_r_r_lrlr(int speed, int repeat){
 
 
+ for (int i = repeat; i > 0; i--){
 
-  for (int i = 2; i >= 0; i--){
+  
     left_half(speed);
-    delay(speed / 5);
-    right_half(speed);
-    delay(speed / 5);     
-    }
+    delay(speed);
+    left_half(speed);
+    delay(speed);
+  
+    right_half(speed, 1);
+    delay(speed);
+    right_half(speed, 1);
+    delay(speed);
   
   
+  
+    for (int i = 2; i >= 0; i--){
+      left_half(speed);
+      delay(speed / 5);
+      right_half(speed, 1);
+      delay(speed / 5);
+      }
+
+ }
+
 }
 
 void sweep(int speed) {
@@ -121,8 +167,8 @@ void sweep(int speed) {
   int id = random(3);
 
   switch (id) {
-    case 0: 
-      split(speed / 2);
+    case 0:
+      split(speed / 2, 9);
       delay(speed);
       break;
     case 1:
@@ -130,21 +176,21 @@ void sweep(int speed) {
       delay(speed);
       break;
     case 2:
-      right_half(speed);
+      right_half(speed, 0);
       delay(speed);
       break;
     case 3:
       flash_all(speed);
       delay(speed);
       break;
-      
-    
+
+
   }
-  
+
 }
 
 
-/* 
+/*
 
   Below are individual flash patterns. You can create new funtions with these flash patterns to create new flash patterns. All
   functions take one parameter. It should be an int. This int will control delay() speeds so the larger the int, the slower the
@@ -153,31 +199,35 @@ void sweep(int speed) {
 */
 
 
-void split(int speed){
+void split(int speed, int repeat){
 
   // LEDS light up in middle, and spread accross
 
-  int num = 1;
+  for (int i = repeat; i > 0; i--){
 
-  for (int i = 7; i >= 0; i--){
-    msr.set_shift(i);
-    msr.set_shift(i + num);
-    num = num + 2;
-    delay(speed);
-  }
-
-
-     for (int k = 0; k <= 15; k++){
-       msr.clear_shift(k);
-
+    int num = 1;
+  
+    for (int i = 7; i >= 0; i--){
+      msr.set_shift(i);
+      msr.set_shift(i + num);
+      num = num + 2;
+      delay(speed);
     }
+  
+  
+       for (int k = 0; k <= 15; k++){
+         msr.clear_shift(k);
+  
+      }
+
+  }
 
 }
 
 void single_right_full(int speed){
 
     // Each LED will turn on from left to right
-    
+
     for (int k = 0; k < 15; k++){
       msr.set_shift(k);
       delay(90);
@@ -199,13 +249,13 @@ void single_right_half(int speed){
 
 void single_left_half(int speed){
 
-  
-  
+
+
   for (int k = 6; k >= 0; k--){
         msr.set_shift(k);
         delay(600);
         msr.clear_shift(k);
-    
+
 
       }
 }
@@ -215,28 +265,34 @@ void single_left_full(int speed){
         msr.set_shift(k);
         delay(600);
         msr.clear_shift(k);
-    
+
 
       }
 }
 
 
-void right_half(int speed){
+void right_half(int speed, int repeat){
 
-  for (int k = 7; k <= 15; k++){
-      msr.set_shift(k);
+  for (int i = repeat; i > 0; i--){
 
-    }
 
-    delay(speed);
- 
- for (int k = 7; k <= 15; k++){
-      msr.clear_shift(k);
 
-    }
+    for (int k = 7; k <= 15; k++){
+        msr.set_shift(k);
+  
+      }
+  
+      delay(speed);
+  
+   for (int k = 7; k <= 15; k++){
+        msr.clear_shift(k);
+  
+      }
+  
+     delay(speed);
 
-   delay(speed);
-    
+  }
+
 }
 
 void left_half(int speed){
@@ -247,14 +303,14 @@ void left_half(int speed){
     }
 
     delay(speed);
- 
+
  for (int k = 0; k <= 6; k++){
       msr.clear_shift(k);
 
     }
 
    delay(speed);
-    
+
 }
 
 void flash_all(int speed){
@@ -265,14 +321,14 @@ void flash_all(int speed){
     }
 
     delay(speed);
- 
+
  for (int k = 0; k <= 15; k++){
       msr.clear_shift(k);
 
     }
 
    delay(speed);
-    
+
 }
 
 void full_stay_right(int speed){
@@ -283,15 +339,15 @@ void full_stay_right(int speed){
   }
 
 for (int i = 0; i < 16; i++){
-  
+
     msr.clear_shift(i);
 
-    
+
   }
 
-    
+
 }
-  
+
 void full_stay_left(int speed){
 
   for (int i = 15; i >= 0; i--){
@@ -302,7 +358,7 @@ void full_stay_left(int speed){
   for (int i = 0; i < 16; i++){
     msr.clear_shift(i);
   }
-  
+
 }
 
 void half_stay_right(){
@@ -312,10 +368,10 @@ void half_stay_right(){
   }
 
 for (int i = 7; i < 16; i++){
-  
+
     msr.clear_shift(i);
 
-    
+
   }
 }
 
@@ -329,8 +385,6 @@ void half_stay_left(){
   for (int i = 0; i < 16; i++){
     msr.clear_shift(i);
   }
-  
+
 }
-
-
 
